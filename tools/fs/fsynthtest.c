949c87940f14c1b4e8d8d80a6aff296b7604d73b
ben <carrotcake1001@gmail.com>
2024-11-01 13:24:42 -0500

    fluidsynth testing

---
+++ tools/fs/fsynthtest.c
@@ -0,0 +1,108 @@
+#include "fluidsynth.h"
+#include <unistd.h>
+fluid_synth_t* synth;
+fluid_audio_driver_t* adriver;
+fluid_sequencer_t* sequencer;
+short synthSeqID, mySeqID;
+unsigned int now;
+unsigned int seqduration;
+ 
+// prototype
+void seq_callback(unsigned int time, fluid_event_t* event, fluid_sequencer_t* seq, void* data);
+ 
+void createsynth() 
+{
+    fluid_settings_t* settings;
+    settings = new_fluid_settings();
+    fluid_settings_setint(settings, "synth.reverb.active", 0);
+    fluid_settings_setint(settings, "synth.chorus.active", 0);
+    synth = new_fluid_synth(settings);
+    adriver = new_fluid_audio_driver(settings, synth);
+    sequencer = new_fluid_sequencer2(0);
+    fluid_synth_set_channel_type(synth, 0, CHANNEL_TYPE_DRUM);
+    fluid_synth_set_channel_type(synth, 1, CHANNEL_TYPE_DRUM);
+    // register synth as first destination
+    synthSeqID = fluid_sequencer_register_fluidsynth(sequencer, synth);
+ 
+    // register myself as second destination
+    mySeqID = fluid_sequencer_register_client(sequencer, "me", seq_callback, NULL);
+ 
+    // the sequence duration, in ms
+    seqduration = 1000;
+}
+ 
+void deletesynth() 
+{
+    delete_fluid_sequencer(sequencer);
+    delete_fluid_audio_driver(adriver);
+    delete_fluid_synth(synth);
+}
+ 
+void loadsoundfont() 
+{
+    int fluid_res;
+    // put your own path here
+    fluid_res = fluid_synth_sfload(synth, "/opt/homebrew/Cellar/fluid-synth/2.4.0/share/fluid-synth/sf2/VintageDreamsWaves-v2.sf2", 1);
+}
+ 
+void sendnoteon(int chan, short key, unsigned int date) 
+{
+    int fluid_res;
+    fluid_event_t *evt = new_fluid_event();
+    fluid_event_set_source(evt, -1);
+    fluid_event_set_dest(evt, synthSeqID);
+    fluid_event_noteon(evt, chan, key, 127);
+    fluid_res = fluid_sequencer_send_at(sequencer, evt, date, 1);
+    delete_fluid_event(evt);
+}
+ 
+void schedule_next_callback() 
+{
+    int fluid_res;
+    // I want to be called back before the end of the next sequence
+    unsigned int callbackdate = now + seqduration/2;
+    fluid_event_t *evt = new_fluid_event();
+    fluid_event_set_source(evt, -1);
+    fluid_event_set_dest(evt, mySeqID);
+    fluid_event_timer(evt, NULL);
+    fluid_res = fluid_sequencer_send_at(sequencer, evt, callbackdate, 1);
+    delete_fluid_event(evt);
+}
+ 
+void schedule_next_sequence() {
+    // Called more or less before each sequence start
+    // the next sequence start date
+    now = now + seqduration;
+ 
+    // the sequence to play
+ 
+    // the beat : 2 beats per sequence
+    sendnoteon(0, 55, now + seqduration/2);
+    sendnoteon(0, 55, now + seqduration);
+ 
+    // melody
+    sendnoteon(1, 55, now + seqduration/10);
+    sendnoteon(1, 55, now + 4*seqduration/10);
+    sendnoteon(1, 55, now + 8*seqduration/10);
+ 
+    // so that we are called back early enough to schedule the next sequence
+    schedule_next_callback();
+}
+ 
+/* sequencer callback */
+void seq_callback(unsigned int time, fluid_event_t* event, fluid_sequencer_t* seq, void* data) {
+    schedule_next_sequence();
+}
+ 
+int main(void) {
+    createsynth();
+    loadsoundfont();
+ 
+    // initialize our absolute date
+    now = fluid_sequencer_get_tick(sequencer);
+    schedule_next_sequence();
+ 
+    sleep(100000);
+    deletesynth();
+    return 0;
+}
